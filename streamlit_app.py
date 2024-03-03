552d6d0075ede22eb576b03194f72e9adb265364
swaroopsakarayapattana <162015675+swaroopsakarayapattana@users.noreply.github.com>
2024-03-03 15:42:22 +0530

    Update streamlit_app.py
    adding menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menue')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Kale Spinach and Rocket Smoothie')
+streamlit.text('Hard Boiled Free-Range Egg')
+streamlit.text('Ragi Muddhe')
+streamlit.text('Uppinkai')
